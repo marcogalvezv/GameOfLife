6f505010903960314b67dfc6c7aa6384a127d336
mgalvez <mgalvez@frontsteps.com>
2024-01-17 22:42:13 -0400

    Add project files.

---
+++ GameOfLife/Controllers/BoardController.cs
@@ -0,0 +1,38 @@
+﻿using GameOfLife.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Protocol;
+
+
+namespace GameOfLife.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BoardController : Controller
+    {
+        // GET: BoardController
+        [HttpGet(Name = "GetBoard")]
+        public string GetBoard()
+        {
+            Board board = new Board(3, 3);
+            board.InitRandomBoard();
+            board.EvaluateBoard();
+            var t = JsonConvert.SerializeObject(board);
+            return t;
+        }
+
+        [HttpPost(Name = "PostBoard")]
+        public string PostBoard(String board)
+        {
+ 
+            Board postedBoard = JsonConvert.DeserializeObject<Board>(board);
+            postedBoard.EvaluateBoard();
+            var t = JsonConvert.SerializeObject(board);
+            return t;
+        }
+
+
+       
+    }
+}
---
+++ GameOfLife/Models/Cell.cs
@@ -0,0 +1,8 @@
+﻿namespace GameOfLife.Models
+{
+    public class Cell
+    {
+        public bool IsLive { get; set; } = false;
+        public List<Cell> Neighborhoods { get; set; }
+    }
+}
